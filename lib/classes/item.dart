6586c50336f57fc969d32aa699e808a3491bbf54
MhmdShamii <mhmddshamii@gmail.com>
2026-01-04 04:27:16 +0200

    uodate fetching items for specific user

--- lib/classes/item.dart
+++ lib/classes/item.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class Item {
   final int id;
   final String name;
@@ -18,5 +21,25 @@ class Item {
       category: json['category'],
       expiryDate: json['expiry_date'],
     );
+  }
+
+  // Static method to fetch items for a user
+  static Future<List<Item>> fetchItems(int userId) async {
+    const String apiUrl =
+        "http://mhmdshami.atwebpages.com/backend/api/get_items.php";
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"user_id": userId}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final items = data['items'] as List;
+      return items.map((json) => Item.fromJson(json)).toList();
+    } else {
+      throw Exception("Failed to fetch items");
+    }
   }
 }
