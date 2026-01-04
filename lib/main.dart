e25bc5520d24475a574821839b3e8504030e58f1
MhmdShamii <mhmddshamii@gmail.com>
2026-01-04 02:40:45 +0200

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Waste Management App',
       debugShowCheckedModeBanner: false,
-      home: Placeholder(),
+      home: Home(),
     );
   }
 }
